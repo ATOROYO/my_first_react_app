ba9def06480f0fcd8bab0b551a5f9decc03ea75e
ATOROYO <davidatoroyosika@gmail.com>
2025-02-10 22:17:21 +0300

    Added async await function

--- App.js
+++ App.js
@@ -1,4 +1,10 @@
-function App() {
+export default function App() {
+  async function getAdvice() {
+    const res = await fetch("https://api.getadviceslip.com/advice");
+    const data = await res.json();
+    console.log(data);
+  }
+
   return (
     <div>
       <h1>Hello I'm Learning React.js!</h1>
