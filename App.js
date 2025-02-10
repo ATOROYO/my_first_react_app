ea2e040ab478215d8960c932b2c4306bb323b787
ATOROYO <davidatoroyosika@gmail.com>
2025-02-10 22:23:47 +0300

    Called the getAdvice function

--- App.js
+++ App.js
@@ -3,18 +3,18 @@
 import { useState } from "react";
 
 export default function App() {
-  const [advice, setState] = useState("");
+  const [advice, setAdvice] = useState("");
 
   async function getAdvice() {
     const res = await fetch("https://api.getadviceslip.com/advice");
     const data = await res.json();
-    console.log(data.slip.advice);
+    setAdvice(data.slip.advice);
   }
 
   return (
     <div>
       <h1>Hello I'm Learning React.js!</h1>
-      <button>Get Advice</button>
+      <button onClick={getAdvice}>Get Advice</button>
     </div>
   );
 }
