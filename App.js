8e3489e4ff6ad4f3bb62f12e4b664fde3d944004
ATOROYO <davidatoroyosika@gmail.com>
2025-02-10 22:30:43 +0300

    Added the useState

--- App.js
+++ App.js
@@ -4,18 +4,20 @@ import { useState } from "react";
 
 export default function App() {
   const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
 
   async function getAdvice() {
     const res = await fetch("https://api.getadviceslip.com/advice");
     const data = await res.json();
     setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
   }
 
   return (
     <div>
       <h1>{advice}</h1>
       <button onClick={getAdvice}>Get Advice</button>
-      <p>You have read {} advices so far</p>
+      <p>You have read {count} advices so far</p>
     </div>
   );
 }
